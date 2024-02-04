d0597960b07891bd2ffd9917ffc0541e2e6e0d3a
yaguarblack1992 <119514366+yaguarblack1992@users.noreply.github.com>
2024-02-04 13:20:36 +0100

    Add files via upload

--- MatrixOperations.cpp
+++ MatrixOperations.cpp
@@ -7,39 +7,50 @@
 
 
 int main() {
-	mx input(4, 4);
-	mx kernel(3, 3);
-	mx target(2, 2);
+	srand(time(NULL));
+	mx input(5, 5);
+	mx kernel1(3, 3), kernel2(3, 3);
+	mx target(1,1);
 
 	input <<
-		1, 0, 1, 0,
-		1, 1, 1, 0,
-		0, 0, 0, 0,
-		1, 1, 1, 1;
+		1, 0, 1, 0, 1,
+		1, 1, 1, 0, 1,
+		0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1,
+		0, 0, 0, 0, 1;
 
-	kernel <<
-		1, 0, 1,
+	kernel1 <<
+		1, 0, -1,
 		0, -1, 0,
-		1, 0, 1;
+		0, 0, 0;
 
 	target <<
-		1.0, 0.5,
-		0.0, 0.0;
+		0.0;
 	
 	// learning process
 	double eta = 0.01;
 	for (int i = 0; i < 100; ++i) {
 		// convolve
-		mx out = convolve(input, kernel);
+		mx out1 = convolve(input, kernel1);
+		mx out2 = convolve(out1, kernel1);
 		// calculate gradient
-		mx grad = out - target;
+		mx grad = out2 - target;
 		// show MSE
 		std::cout << "Iteration " << i << " MSE= " << grad.array().square().sum() << '\n';
-		// calculate delta
-		mx delta = deltaKernel(input, kernel, grad);
+		// calculate delta in each steps
+
+		mx delta2 = deltaKernel(out1, kernel1, grad);
+		// calculate new grad
+		auto grad1 = gradientTransfer(kernel1, grad);
+		mx delta1 = deltaKernel(input, kernel1, grad1);
 		// update weights
-		kernel = kernel - delta * eta;
+		kernel1 = kernel1 - delta1 * eta - delta2 * eta;
 	}
+
+	std::cout << kernel1 << "\n\n" << "\n\n";
+
+	std::cout << convolve(input, kernel1) << "\n\n";
+	std::cout << convolve(convolve(input, kernel1), kernel1);
 
 	return 0;
 }
