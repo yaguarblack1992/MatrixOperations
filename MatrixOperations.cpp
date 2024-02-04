a65cc3feb362441a7124d4fc4e54e38f1458aa38
yaguarblack1992 <119514366+yaguarblack1992@users.noreply.github.com>
2024-02-04 14:19:00 +0100

    Add files via upload

--- MatrixOperations.cpp
+++ MatrixOperations.cpp
@@ -3,7 +3,7 @@
 #include <Eigen/Dense>
 #include "Convolutions.h"
 
-
+std::vector <mx> patterns;
 
 
 int main() {
@@ -23,12 +23,42 @@ int main() {
 
 	target <<
 		1.0;
-	
-	// learning process
+	// vector with patterns
+	mx input1(5, 5), input2(5, 5), input3(5, 5);
+	input1 <<
+		0, 0, 0, 0, 0,
+		1, 1, 1, 0, 0,
+		0, 0, 1, 1, 1,
+		1, 1, 1, 0, 0,
+		0, 0, 0, 0, 0;
+	input2 <<
+		0, 0, 0, 0, 0,
+		0, 0, 1, 1, 1,
+		1, 1, 1, 0, 0,
+		0, 0, 1, 1, 1,
+		0, 0, 0, 0, 0;
+	input3 <<
+		0, 1, 0, 1, 0,
+		0, 1, 0, 1, 0,
+		0, 1, 1, 1, 0,
+		0, 0, 1, 0, 0,
+		0, 0, 1, 0, 0;
+
+	patterns = { input,input1,input2,input3 };
+
+	// gonna try to recognize pattern no matter of its orientation
+	// some parameters
+	int iterations = 300;
 	double eta = 0.01;
-	for (int i = 0; i < 100; ++i) {
+
+	std::cout << "Set parameters\n" << "Iterations ";
+	std::cin >> iterations;
+	std::cout << "Learning rate ";
+	std::cin >> eta;
+	// learning process
+	for (int i = 0; i < iterations; ++i) {
 		// convolve
-		mx out1 = convolve(input, kernel);
+		mx out1 = convolve(patterns[i%4], kernel);
 		mx out2 = convolve(out1, kernel);
 		// calculate gradient
 		mx grad = out2 - target;
@@ -39,7 +69,7 @@ int main() {
 		mx delta2 = deltaKernel(out1, kernel, grad);
 		// calculate new grad
 		auto grad1 = gradientTransfer(kernel, grad);
-		mx delta1 = deltaKernel(input, kernel, grad1);
+		mx delta1 = deltaKernel(patterns[i%4], kernel, grad1);
 		// update weights
 		kernel = kernel - delta1 * eta - delta2 * eta;
 	}
@@ -50,13 +80,46 @@ int main() {
 	std::cout << "Convolution 2\n";
 	std::cout << convolve(convolve(input, kernel), kernel) << "\n\n";
 
-	mx kernel5(5, 5);
-	kernel5.setRandom();
-	std::cout << "kernel 5x5 random\n" << kernel5 << "\n\n";
-	std::cout << "convolution with random kernel\n" << convolve(input, kernel5) << "\n\n";
-	std::cout << "Kernel 5x5 learned\n";
-	mx kernel5_learned = learnKernel(input, kernel5, target, 200, 0.01);
-	std::cout << kernel5_learned << "\n\n";
-	std::cout << "convolved:\n" << convolve(input, kernel5_learned);
+	
+	// now we add some random matrices to learning process
+	std::cout << "With random noisy matrices\n\n";
+	
+	for (int i = 0; i < iterations; ++i) {
+		// generate random matrix each second iteration and use as input, set target accordingly
+		mx input_matrix(patterns[0]);
+		if (i % 2 == 0)
+			input_matrix = patterns[rand() % 4], target << 1.0;
+		else
+			input_matrix.setRandom(), target << 0.0;
+		// convolve
+		mx out1 = convolve(input_matrix, kernel);
+		mx out2 = convolve(out1, kernel);
+		// calculate gradient
+		mx grad = out2 - target;
+		// show MSE
+		std::cout << "Iteration " << i << " MSE= " << grad.array().square().sum() << '\n';
+		// calculate delta in each steps
+
+		mx delta2 = deltaKernel(out1, kernel, grad);
+		// calculate new grad
+		auto grad1 = gradientTransfer(kernel, grad);
+		mx delta1 = deltaKernel(input_matrix, kernel, grad1);
+		// update weights
+		kernel = kernel - delta1 * eta - delta2 * eta;
+	}
+
+	std::cout << "Learned kernel\n";
+	std::cout << kernel << "\n\n" << "\n\n";
+	std::cout << "Convolution 1\n";
+	std::cout << convolve(input, kernel) << "\n\n";
+	std::cout << "Convolution 2\n";
+	std::cout << convolve(convolve(input, kernel), kernel) << "\n\n";
+	mx random(5, 5);
+	random.setRandom();
+	std::cout << "Convolution with random matrix\n";
+	std::cout << convolve(random, kernel) << "\n\n";
+	std::cout << "Convolution 2 with random matrix\n";
+	std::cout << convolve(convolve(random, kernel), kernel) << "\n\n";
+
 	return 0;
 }
